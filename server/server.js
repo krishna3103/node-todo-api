25d0867d5618492dbef7a9ed42d61e6e4669d3ea
Krishna Gupta <krishna.gupta@rocketmail.com>
2018-08-11 15:16:56 +0530

    added middleware for authenticate

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const {ObjectID}  	= require('mongodb');
 var {mongoose} 	= require('./db/mongoose');
 var {Todo} 		= require('./models/todo');
 var {User} 		= require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
 
 var app = express();
 
@@ -132,16 +134,8 @@ app.post('/users',(req, res) => {
 
 //Check user middleware
 
-app.get('/users/me', (req, res) => {
-	var token = req.header('x-auth');
-	User.findByToken(token).then((user) => {
-		if(!user){
-			return Promise.reject();
-		}
-		res.send(user);
-	}).catch ((e) => {
-		res.status(401).send();
-	});
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
